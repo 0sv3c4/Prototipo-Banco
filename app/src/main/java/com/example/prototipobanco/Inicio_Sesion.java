d0001039d0b10134a52b524160244ebb991cfece
oscar <otn1001@alu.ubu.es>
2026-04-19 18:09:25 +0200

    Control de DNI

--- app/src/main/java/com/example/prototipobanco/Inicio_Sesion.java
+++ app/src/main/java/com/example/prototipobanco/Inicio_Sesion.java
@@ -5,6 +5,7 @@ import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
+
 public class Inicio_Sesion extends AppCompatActivity {
+
+    public static final int NUM_NIF=8;
+
+    //Valores de la letra que debe tener el dni según su módulo
+    private enum LetraDNI {T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +52,38 @@ public class Inicio_Sesion extends AppCompatActivity {
 
             // Mantiene el cursor al final del texto cuando se cambia
             escribeContra.setSelection(escribeContra.getText().length());
-        });
+        }
+        );
 
+
+        //Asegurar inicio de sesión correcto
+        MaterialButton btnIniciarSesion = findViewById(R.id.btn_iniciar_sesion);
+        EditText escribeNIF = findViewById(R.id.introduzca_NIF);
+
+        btnIniciarSesion.setOnClickListener(v ->{
+            String nifRevisar = escribeNIF.getText().toString().trim();//quedarnos con los digitos solo
+            boolean datoErroneo =false;
+            int numeroDni;
+            try{
+                if(nifRevisar.length()!=NUM_NIF+1) {
+                    datoErroneo=true; //debe tener 8 dígitos más la letra
+                } else {
+                    numeroDni = Integer.parseInt(nifRevisar.substring(0,8));
+                    String letra = nifRevisar.substring(8);
+                    if(!LetraDNI.values()[numeroDni%23].toString().equals(letra)){
+                        datoErroneo=true;
+                    }
+                }
+            } catch(NumberFormatException e){ //iba a utilizar _, pero no deja entiendo por la versión de java
+                datoErroneo=true;
+            }
+
+            if(datoErroneo){
+                Toast.makeText(this, "El formato del DNI NO es correcto", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "DNI válido ✓", Toast.LENGTH_LONG).show();
+            }
+        }
+        );
     }
 }
