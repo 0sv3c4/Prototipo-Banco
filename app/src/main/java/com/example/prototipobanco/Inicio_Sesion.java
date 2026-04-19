e9be7129b0e148de2b80732a620c4630279c37ba
oscar <otn1001@alu.ubu.es>
2026-04-19 14:10:40 +0200

    1º intento de enlazar dos activities

--- app/src/main/java/com/example/prototipobanco/Inicio_Sesion.java
+++ app/src/main/java/com/example/prototipobanco/Inicio_Sesion.java
@@ -1,6 +1,8 @@
 package com.example.prototipobanco;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,13 @@ public class Inicio_Sesion extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnIniciarSesion = findViewById(R.id.inicio_sesion);
+
+        btnIniciarSesion.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Inicio_Sesion.class);
+            startActivity(intent);
         });
     }
 }
