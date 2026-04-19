6efbc935ad762f32c2c253034946fb1e426f6f22
oscar <otn1001@alu.ubu.es>
2026-04-19 16:55:07 +0200

    Pantalla con opción de visibilidad de la contraseña y correcciones menores

--- app/src/main/java/com/example/prototipobanco/Inicio_Sesion.java
+++ app/src/main/java/com/example/prototipobanco/Inicio_Sesion.java
@@ -1,8 +1,10 @@
 package com.example.prototipobanco;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +23,26 @@ public class Inicio_Sesion extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Atts modificación contraseña
+        EditText escribeContra = findViewById(R.id.introduzca_contra);
+        ImageView btnVerContra = findViewById(R.id.btn_ver_contrasena);
+
+        /*Función para la modification de la visibilidad de la contraseña.*/
+        btnVerContra.setOnClickListener(v -> {
+            if (escribeContra.getTransformationMethod() instanceof PasswordTransformationMethod) {
+                // Contraseña oculta -> mostrarla
+                escribeContra.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                btnVerContra.setImageResource(R.drawable.ic_ojo_abierto);
+            } else {
+                // Contraseña visible -> ocultarla
+                escribeContra.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                btnVerContra.setImageResource(R.drawable.ic_ojo_cerrado);
+            }
+
+            // Mantiene el cursor al final del texto cuando se cambia
+            escribeContra.setSelection(escribeContra.getText().length());
         });
 
     }
