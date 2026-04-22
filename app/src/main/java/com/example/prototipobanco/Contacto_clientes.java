64b29e4b44c299ed29ff62f187892bdd658634b0
oscar <otn1001@alu.ubu.es>
2026-04-22 15:24:43 +0200

    - Correcciones menores

--- app/src/main/java/com/example/prototipobanco/Contacto_clientes.java
+++ app/src/main/java/com/example/prototipobanco/Contacto_clientes.java
@@ -14,7 +14,7 @@ public class Contacto_clientes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.contacto_clientes);
+        setContentView(R.layout.activity_contacto_clientes);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
