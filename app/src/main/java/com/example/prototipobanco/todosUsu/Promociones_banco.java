3fc29b7fd60a5f77a4f1a7eb0757a8f718759358
oscar <otn1001@alu.ubu.es>
2026-04-25 16:40:17 +0200

    - Creación de la actividad promociones

---
+++ app/src/main/java/com/example/prototipobanco/todosUsu/Promociones_banco.java
@@ -0,0 +1,31 @@
+package com.example.prototipobanco.todosUsu;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.prototipobanco.R;
+
+public class Promociones_banco extends BaseActivityTodos {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_promociones_banco);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnChat = findViewById(R.id.btn_chat_bottom);
+        btnAtras = findViewById(R.id.btn_volver);
+
+        btnAtras.setOnClickListener(v -> finish());
+        btnChat.setOnClickListener(v -> generarChat());
+    }
+}
