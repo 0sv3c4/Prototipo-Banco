05057362d958a9137a1352ee3bc801bce54ad8cc
oscar <otn1001@alu.ubu.es>
2026-05-12 15:29:41 +0200

    - Intraducibles y otras cosas

--- app/src/main/java/com/example/prototipobanco/todosUsu/Pantalla_inicial.java
+++ app/src/main/java/com/example/prototipobanco/todosUsu/Pantalla_inicial.java
@@ -2,7 +2,6 @@ package com.example.prototipobanco.todosUsu;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
